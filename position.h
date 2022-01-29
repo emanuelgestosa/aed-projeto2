a10d6ae154284fee320a95ab81f79e8f60d0ef8c
AngoIa <emanuel-367@hotmail.com>
2022-01-29 13:53:43 +0000

    commenting the position

--- position.h
+++ position.h
@@ -7,11 +7,42 @@ class Position {
 private:
     double lat, lon;
 public:
+    /**
+     * Calculate the distance between two
+     * positions using Haversine's formula.
+     * 
+     * @param pos2 The position to calculate the distance to.
+     * 
+     * @return The distance between the two points in km.
+     */
     double calcDist(const Position& pos2) const;
 
+    /**
+     * Sets the latitute.
+     * 
+     * @param lat The new latitutde.
+     */
     void setLat(const double lat);
+
+    /**
+     * Sets the longitude.
+     * 
+     * @param lon The new longitude.
+     */
     void setLon(const double lon);
+
+    /**
+     * Gets the latitude.
+     * 
+     * @return The latitude.
+     */
     double getLat() const;
+
+    /**
+     * Gets the longitude.
+     * 
+     * @return The new longitude.
+     */
     double getLon() const;
 };
 
